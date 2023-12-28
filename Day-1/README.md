a3adce239b22278b09c87a2891fc4e8da538f64a
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-28 09:21:02 +0600

    learned array lessons

--- Day-1/README.md
+++ Day-1/README.md
@@ -1,1 +1,37 @@
 # Day-1
+
+### Lesson-1
+
+---> array.length
+
+- helps to find the length of an array
+
+### Lesson-2
+
+---> array.toString()
+
+- converts an array element to comma separated string
+
+### Lesson-3
+
+---> array.pop()
+
+- helps to remove last element from an array
+
+### Lesson-4
+
+---> array.push(elem)
+
+- helps to add new element to an array
+
+### Lesson-5
+
+---> array.shift()
+
+- helps to remove first element from an array
+
+### Lesson-4
+
+---> array.push(elem)
+
+- helps to add new element to an array in the beginning
---
+++ Day-1/index.js
@@ -0,0 +1,31 @@
+// array.length
+
+const fruits0 = ["banana", "coconut", "apple"]
+const result0 = fruits0.length
+// console.log(total)
+
+// array.toString()
+const fruits1 = ["banana", "coconut", "apple"]
+const result1 = fruits1.toString()
+// console.log(result1)
+
+// array.pop()
+const fruits2 = ["banana", "coconut", "apple"]
+const result2 = fruits2.pop()
+// console.log(fruits2)
+
+
+// array.push()
+const fruits3 = ["banana", "coconut", "apple"]
+const result3 = fruits3.push("orange")
+// console.log(fruits3)
+
+// array.shift()
+const fruits4 = ["banana", "coconut", "apple"]
+const result4 = fruits4.shift()
+// console.log(fruits4)
+
+// array.unShift()
+const fruits5 = ["banana", "coconut", "apple"]
+const result5= fruits5.unshift("mango")
+console.log(fruits5)
