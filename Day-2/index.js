bf6cf4439f823f351be92e67deefe7cf2ea0fda6
Mahdi Mortuza <mahdi.webx@gmail.com>
2023-12-29 18:41:01 +0600

    concat, flat,splice,slice

---
+++ Day-2/index.js
@@ -0,0 +1,28 @@
+// array.concat()
+
+const fruits1 = ["banana", "coconut", "apple"]
+const fruits2 = ["kiwi", "pineapple", "grave"]
+
+// const result = fruits1.concat(fruits2)
+// console.log(result)
+
+
+
+//array.flat()
+const myNumber = [[1,2], [3,4], [4, 5], [6, 7]]
+
+// const result = myNumber.flat()
+// console.log(result)
+
+
+// array.splice()
+const fruitsSplice = ["banana", "coconut", "apple", "kiwi", "pineapple", "grave"]
+
+fruitsSplice.splice(0, 2,)
+// console.log(fruitsSplice)
+
+
+// array.slice()
+const fruitsSlice = ["banana", "coconut", "apple", "kiwi", "pineapple", "grave"]
+const result = fruitsSlice.slice(1, 3)
+console.log(result)
